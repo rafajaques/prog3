c1e0b5848af420a79022992a2413a87511482792
Rafael Jaques <rafael@phpit.com.br>
2021-10-25 21:07:41 -0300

    Adicionadas as migrations

---
+++ database/migrations/2021_10_25_205709_create_produtos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 255);
+            $table->decimal('preco', 8, 2);
+            $table->text('descricao');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
--- database/migrations/2021_10_25_205720_create_usuarios_table.php
+++ database/migrations/2021_10_25_205720_create_usuarios_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateUsuariosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 255);
+            $table->string('email', 255);
+            $table->string('usuario', 255);
+            $table->string('senha', 255);
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('usuarios');
     }
 }
